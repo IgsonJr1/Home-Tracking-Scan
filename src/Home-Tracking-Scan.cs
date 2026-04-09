9df80f5fc8a7ecb02cd0970b49f09e85023bb592
IgsonJr1 <igson.com@hotmail.com>
2026-04-09 10:29:21 -0400

    Update Home-Tracking-Scan.cs

--- src/Home-Tracking-Scan.cs
+++ src/Home-Tracking-Scan.cs
@@ -11,13 +11,13 @@ namespace PKHeX.Plugin
     {
         public string Name => "Home Tracking Scan";
         public int Priority => 1;
-        public ISaveFileProvider SaveFileEditor { get; private set; }
+        public ISaveFileProvider SaveFileEditor { get; private set; } = null!;
 
         public void Initialize(params object[] args)
         {
-            SaveFileEditor = (ISaveFileProvider)Array.Find(args, z => z is ISaveFileProvider);
-            var menu = (ToolStrip)Array.Find(args, z => z is ToolStrip);
-            var toolsMenu = (ToolStripMenuItem)menu.Items["Menu_Tools"];
+            SaveFileEditor = (ISaveFileProvider)Array.Find(args, z => z is ISaveFileProvider)!;
+            var menu = (ToolStrip)Array.Find(args, z => z is ToolStrip)!;
+            var toolsMenu = (ToolStripMenuItem)menu.Items["Menu_Tools"]!;
 
             var exportItem = new ToolStripMenuItem("Exportar Trackers das Boxes");
             exportItem.Click += (s, e) => ExportarDados();
@@ -42,17 +42,27 @@ namespace PKHeX.Plugin
                     var pkm = sav.GetBoxSlotAtIndex(i, j);
                     if (pkm.Species <= 0) continue;
 
+                    // Formata o valor do Tracker (u64) em Hexadecimal de 16 dígitos
                     string trackerHex = pkm.Tracker.ToString("X16");
                     results.Add($"{i + 1},{j + 1},{pkm.Species},{pkm.Nickname},{trackerHex}");
                 }
             }
 
             string fileName = "trackers_home.csv";
-            File.WriteAllLines(fileName, results);
-            MessageBox.Show($"Relatório '{fileName}' gerado com sucesso!", "Sucesso");
+            try 
+            {
+                File.WriteAllLines(fileName, results);
+                MessageBox.Show($"Relatório '{fileName}' gerado com sucesso na pasta do seu PKHeX!", "Sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar arquivo: {ex.Message}");
+            }
         }
 
+        // Métodos exigidos pela interface IPlugin da versão 26.3.20+
         public void NotifySaveLoaded() { }
-        public bool TryModifyFile(string path) => false;
+        public bool TryLoadFile(string filePath) => false; 
+        public bool TryModifyFile(string filePath) => false;
     }
 }
